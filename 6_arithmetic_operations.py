93b3ce1a00051455b320e61e8ea5f0622a50297d
islemwilcher <islemadobe6@gmail.com>
2022-08-27 03:13:53 +0100

    Arithmetic operators are used with numeric values to perform common mathematical operations

---
+++ 6_arithmetic_operations.py
@@ -0,0 +1,24 @@
+
+# Addition
+x = 3 + 3
+print(x) # 6
+
+# Division
+x1 = 10 / 3
+print(x1) # 3.3333...
+
+# Floor division
+x2 = 10 // 3
+print(x2) # 3
+
+# Modulus
+x3 = 10 % 3
+print(x3) # 1
+
+# Multiplication
+x4 = 3 * 3
+print(x4) # 9
+
+# Exponentiation 
+x5 = 3 ** 3
+print(x5) # 27
