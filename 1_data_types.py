3788c565747b6980c3aeb1596ac73a2d58f23957
islemwilcher <islemadobe6@gmail.com>
2022-08-27 02:22:03 +0100

    data types you must know

---
+++ 1_data_types.py
@@ -0,0 +1,30 @@
+
+# data types you need to know
+# int =>
+age = 24
+# float =>
+height = 1.88
+# string =>
+name = 'islem'
+# boolean =>
+is_software_engineer = True
+# list =>
+list_data = ['one', 'two', 'three']
+# tuple =>
+tuple_data = ('one', 'two', 'three')
+# set =>
+set_data = {'one', 'two', 'three'}
+
+###########################
+# print in the console
+print('name:', name)
+print('age:', age)
+print('height:', height)
+print('is software engineer:', is_software_engineer)
+print('list data:', list_data)
+print('tuple data:', tuple_data)
+print('set data:', set_data)
+
+###########################
+# concatenate
+print(f'my name: {name}, i am {age} years old and my tall is {height}.')
